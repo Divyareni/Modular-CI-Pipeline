2e37ae66f15d48d6a3ceaaf715efad3eea419242
Divya <divya8renita@gmail.com>
2026-04-06 13:40:19 +1000

    Update hardware_controller.hpp

--- src/hardware_controller.hpp
+++ src/hardware_controller.hpp
@@ -1,20 +1,15 @@
 #pragma once
 #include <string>
-#include <iostream>
 
 class HardwareController {
-    public:
-          HardwareController(int device_id) : id(device_id) {}
+public:
+    // Only declarations here (no { } blocks with logic)
+    HardwareController(int device_id);
 
-          void write_register(int address, int value) {
-              std::cout << "Device " << id << ": Writing " << value 
-                  << " to address " << std::hex << address << std::endl;
-          }
+    void write_register(int address, int value);
 
-          std::string get_status() {
-              return "Device " + std::to_string(id) + " is Operational";
-          }
+    std::string get_status();
 
-    private:
-         int id;
+private:
+    int id;
 };
